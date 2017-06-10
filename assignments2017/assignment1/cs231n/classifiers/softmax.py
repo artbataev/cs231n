2ddd808f99eb60d96f666dca9f3f6a6c6e9f3e69
artbataev <artbataev@gmail.com>
2017-06-11 02:05:41 +0300

    Softmax vectorized derivative implemented

--- assignments2017/assignment1/cs231n/classifiers/softmax.py
+++ assignments2017/assignment1/cs231n/classifiers/softmax.py
@@ -37,7 +37,7 @@ def softmax_loss_naive(W, X, y, reg):
         # loss += -np.log(np.exp(current_logits[y[i]]) / np.sum(np.exp(current_logits)))
         ###
         log_c_coeff = np.max(current_logits)
-        corrected_logits = current_logits - log_c_coeff # correction to avoid large numbers
+        corrected_logits = current_logits - log_c_coeff  # correction to avoid large numbers
         logits_exp = np.exp(corrected_logits)
         sum_logits_exp = np.sum(logits_exp)
         log_sum_logits_exp = np.log(sum_logits_exp)
@@ -52,15 +52,15 @@ def softmax_loss_naive(W, X, y, reg):
         d_log_sum_logits_exp = 1.0
         d_corrected_logits[y[i]] += -1 * d_inverted_true_logit
         d_sum_logits_exp = d_log_sum_logits_exp * (1 / sum_logits_exp)
-        d_logits_exp = np.full_like(corrected_logits, d_sum_logits_exp) # distribute gradient
+        d_logits_exp = np.full_like(corrected_logits, d_sum_logits_exp)  # distribute gradient
         d_corrected_logits += d_logits_exp * logits_exp
 
         d_current_logits = d_corrected_logits
         d_current_logits[np.argmax(current_logits)] -= np.sum(d_current_logits)
         current_dW = np.zeros_like(dW)
-            # for j in range(num_features):
-            #     for k in range(num_classes):
-            #         current_dW[j, k] = d_current_logits[k] * X[i, j]
+        # for j in range(num_features):
+        #     for k in range(num_classes):
+        #         current_dW[j, k] = d_current_logits[k] * X[i, j]
         for k in range(num_classes):
             current_dW[:, k] = d_current_logits[k] * X[i]
         ###
@@ -69,6 +69,7 @@ def softmax_loss_naive(W, X, y, reg):
     dW /= num_train
     loss /= num_train
 
+    # regularization
     loss += reg * np.sum(np.square(W))
     dW += 2 * reg * W
     #############################################################################
@@ -95,13 +96,34 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     num_train = X.shape[0]
+    num_classes = W.shape[1]
     logits = X.dot(W)
     log_c_coeff = np.max(logits, axis=1)
-    logits -= np.reshape(log_c_coeff, (-1, 1))
-    scores = - (logits[np.arange(num_train), y] - np.log(np.sum(np.exp(logits), axis=1)))
-    loss = np.sum(scores) / num_train
+    corrected_logits = logits - np.reshape(log_c_coeff, (-1, 1))
+    logits_exp = np.exp(corrected_logits)
+    sum_logits_exp = np.sum(logits_exp, axis=1)
+    log_sum_logits_exp = np.log(sum_logits_exp)
+    inverted_true_logits = -1 * corrected_logits[np.arange(num_train), y]
+    current_loss = inverted_true_logits + log_sum_logits_exp
+    sum_loss = np.sum(current_loss)
+    loss = sum_loss / num_train
 
-    # dW not implemented
+    d_corrected_logits = np.zeros_like(corrected_logits)
+    d_inverted_true_logits = np.ones_like(inverted_true_logits)
+    d_log_sum_logits_exp = np.ones_like(log_sum_logits_exp)
+    d_corrected_logits[np.arange(num_train), y] += -1 * d_inverted_true_logits
+    d_sum_logits_exp = d_log_sum_logits_exp * (1 / sum_logits_exp)
+    d_logits_exp = np.zeros_like(corrected_logits)  # distribute gradient
+    for j in range(d_logits_exp.shape[1]):
+        d_logits_exp[:, j] = d_sum_logits_exp
+    d_corrected_logits += d_logits_exp * logits_exp
+
+    d_logits = d_corrected_logits
+    d_logits[np.arange(num_train), np.argmax(logits, axis=1)] -= np.sum(d_logits, axis=1)
+
+    dW = X.T.dot(d_logits)
+    dW /= num_train
+    # regularization
     loss += reg * np.sum(np.square(W))
     dW += 2 * reg * W
     #############################################################################
